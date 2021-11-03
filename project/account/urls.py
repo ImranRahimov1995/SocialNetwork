d743afb02c0208bc021579bea0fa2ea2b3b66f08
ImranRahimov1995 <imash.odessahouse@gmail.com>
2021-11-03 14:50:21 +0400

    ADD password change based on DjandoAuthViews

--- project/account/urls.py
+++ project/account/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     #path('login/',user_login,name='login'),
     path('login/',av.LoginView.as_view(),name='login'),
     path('logout/',av.LogoutView.as_view(),name='logout'),
+    path('password_change/',av.PasswordChangeView.as_view(),name='password_change'),
+    path('password_change/done/',av.PasswordChangeDoneView.as_view(),name='password_change_done'),
     path('',dashboard,name='dashboard'),
 ]
