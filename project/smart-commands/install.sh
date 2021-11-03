1ab47279496e854fe495cf6378d9e89f93908b89
ImranRahimov1995 <imash.odessahouse@gmail.com>
2021-11-03 13:30:32 +0400

    Start project with minimal settigns , add app account

---
+++ project/smart-commands/install.sh
@@ -0,0 +1,11 @@
+export DJANGO_SETTINGS_MODULE=config.settings.local
+
+
+cd ..
+python3 -m venv venv
+source venv/bin/activate
+cd project/
+
+pip3 install -r requirements.txt
+python3 manage.py migrate --settings=config.settings.local
+python3 manage.py collectstatic --noinput
