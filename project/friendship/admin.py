27705a6a6d665c9489185226b45f5566e40f6dc7
ImranRahimov1995 <imash.odessahouse@gmail.com>
2021-11-22 12:36:58 +0400

    Changed friendship information save

--- project/friendship/admin.py
+++ project/friendship/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FriendshipRequest
+from .models import FriendshipRequest,Friends
 
 
 admin.site.register(FriendshipRequest)
+admin.site.register(Friends)
